bd413afb7886248c36fc1ba63f959662b8e070db
Benjamin Pasero <benjpas@microsoft.com>
2018-08-11 09:46:04 +0200

    compile against node 10

--- src/mutex.cc
+++ src/mutex.cc
@@ -67,7 +67,7 @@ NAN_METHOD(Mutex::New) {
 		const int argc = 1;
 		v8::Local<v8::Value> argv[argc] = { info[0] };
 		v8::Local<v8::Function> cons = Nan::New(constructor);
-		info.GetReturnValue().Set(cons->NewInstance(argc, argv));
+		info.GetReturnValue().Set(Nan::NewInstance(cons, argc, argv).ToLocalChecked());
 	}
 }
 
